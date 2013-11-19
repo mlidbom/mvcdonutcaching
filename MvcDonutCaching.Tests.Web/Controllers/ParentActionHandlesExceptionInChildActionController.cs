91617c5d60d3d1c1e33f51257c7a8d074eea3756
mlidbo <magnus.lidbom@gmail.com>
2013-11-19 16:09:54 +0100

    Try like crazy, but fail, to reproduce a bug that occurred with version 1.3.0-mp3 in our application.

---
+++ MvcDonutCaching.Tests.Web/Controllers/ParentActionHandlesExceptionInChildActionController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Web.Mvc;
+using DevTrends.MvcDonutCaching.Mlidbom;
+
+namespace MvcDonutCaching.Tests.Web.Controllers
+{
+    [AutoOutputCache(Duration = .1)]
+    public class ParentActionHandlesExceptionInChildActionController : Controller
+    {
+        private ActionResult DelayAndDisplay(ActionResult result)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(1));
+            return result;
+        }
+
+        public ActionResult Index() { return DelayAndDisplay(View()); }
+
+        public ActionResult TopLayoutHeader() { return DelayAndDisplay(View()); }
+
+        public ActionResult TopLayoutFooter() { return DelayAndDisplay(View()); }
+
+        public ActionResult NestedLayoutHeader() { return DelayAndDisplay(View()); }
+
+        public ActionResult NestedLayoutFooter() { return DelayAndDisplay(View()); }
+
+        public ActionResult PageBody() { return DelayAndDisplay(View()); }
+
+        public ActionResult SwallowExceptionInNestedAction() { return DelayAndDisplay(View()); }
+
+        public ActionResult SwallowExceptionInNestedView() { return DelayAndDisplay(View()); }
+
+        public ActionResult SwallowExceptions() { return DelayAndDisplay(View()); }
+
+        public ActionResult SwallowChildActionException() { return DelayAndDisplay(View()); }
+
+        public ActionResult SwallowChildViewException() { return DelayAndDisplay(View()); }
+
+
+        [AutoOutputCache(Duration = 0)]
+        public ActionResult ThrowExceptionInView() { return DelayAndDisplay(View()); }
+
+        public ActionResult ThrowInNestedChildView() { return DelayAndDisplay(View()); }
+        
+        public ActionResult ThrowInNestedChildAction() { return DelayAndDisplay(View()); }
+
+        [AutoOutputCache(Duration = 0)]
+        public ActionResult ThrowExceptionInAction()
+        {
+            throw new Exception("ExceptionTextThatShouldNeverBeShown");
+        }
+    }
+}
---
+++ MvcDonutCaching.Tests/Mlidbom/PageThatSwallowsExceptionInChildActionsAndViews.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Web.Mvc;
+using NCrunch.Framework;
+using NUnit.Framework;
+
+namespace MvcDonutCaching.Tests.Mlidbom
+{
+    public abstract class PageThatSwallowsExceptionInChildActionsAndViews : ControllerTestBase
+    {
+        private const string Controller = "ParentActionHandlesExceptionInChildAction";
+        override protected string ControllerName { get { return Controller; } }
+
+        protected abstract string ActionName { get; }
+
+        public override void RunAfterEachTest()
+        {
+            ClearActionCache(Controller, ActionName);
+        }
+
+        [Test]
+        public void DoesNotCrashOnInitialRender()
+        {
+            ExecuteAction(ActionName);
+        }
+
+        [Test]
+        public void SubsequentCallsRenderIdenticalContent()
+        {
+            var initialContent = ExecuteAction(ActionName);
+
+            long requestsMade = 0;
+
+            var runUntil = DateTime.Now + TimeSpan.FromSeconds(1);
+
+            while(DateTime.Now < runUntil)
+            {
+                ++requestsMade;
+                var newContent = ExecuteAction(ActionName);
+                Assert.That(newContent, Is.EqualTo(initialContent));
+            }
+
+            Console.WriteLine("Made {0} requests with no failure");
+        }
+    }
+
+    [ExclusivelyUses("ParentActionHandlesExceptionInChildAction_Index")]
+    public class PageThatSwallowsExceptionInChildActionsAndViews_Index : PageThatSwallowsExceptionInChildActionsAndViews
+    {
+        override protected string ActionName { get { return "Index"; } }
+    }
+
+    [ExclusivelyUses("ParentActionHandlesExceptionInChildAction_SwallowChildActionException")]
+    public class PageThatSwallowsExceptionInChildActionsAndViews_SwallowChildActionException : PageThatSwallowsExceptionInChildActionsAndViews
+    {
+        override protected string ActionName { get { return "SwallowChildActionException"; } }
+    }
+
+    [ExclusivelyUses("ParentActionHandlesExceptionInChildAction_SwallowChildViewException")]
+    public class PageThatSwallowsExceptionInChildActionsAndViews_SwallowChildViewException : PageThatSwallowsExceptionInChildActionsAndViews
+    {
+        override protected string ActionName { get { return "SwallowChildViewException"; } }
+    }
+
+    [ExclusivelyUses("ParentActionHandlesExceptionInChildAction_SwallowExceptionInNestedAction")]
+    public class PageThatSwallowsExceptionInChildActionsAndViews_SwallowExceptionInNestedAction : PageThatSwallowsExceptionInChildActionsAndViews
+    {
+        override protected string ActionName { get { return "SwallowExceptionInNestedAction"; } }
+    }
+
+    [ExclusivelyUses("ParentActionHandlesExceptionInChildAction_SwallowExceptionInNestedView")]
+    public class PageThatSwallowsExceptionInChildActionsAndViews_SwallowExceptionInNestedView : PageThatSwallowsExceptionInChildActionsAndViews
+    {
+        override protected string ActionName { get { return "SwallowExceptionInNestedView"; } }
+    }
+
+    [ExclusivelyUses("ParentActionHandlesExceptionInChildAction_SwallowExceptions")]
+    public class PageThatSwallowsExceptionInChildActionsAndViews_SwallowExceptions : PageThatSwallowsExceptionInChildActionsAndViews
+    {
+        override protected string ActionName { get { return "SwallowExceptions"; } }
+    }
+}
